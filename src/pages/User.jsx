1b7fd031c3f4e316259514662012e619149cde2b
Jon Widener <jon.widener@gmail.com>
2022-02-15 19:03:09 -0600

    Finished section 11

--- src/pages/User.jsx
+++ src/pages/User.jsx
@@ -5,17 +5,20 @@ import { useParams } from 'react-router-dom';
 import Spinner from '../components/layout/Spinner';
 import GithubContext from '../context/github/GithubContext';
 import RepoList from '../components/repos/RepoList';
+import { getUserAndRepos } from '../context/github/GithubActions';
 
 function User() {
-  const { getUser, user, getUserRepos, repos, loading } =
-    useContext(GithubContext);
+  const { user, repos, loading, dispatch } = useContext(GithubContext);
 
   const params = useParams();
 
   useEffect(() => {
-    getUser(params.login);
-    getUserRepos(params.login);
-  }, []);
+    dispatch({ type: 'SET_LOADING' });
+    (async () => {
+      const userData = await getUserAndRepos(params.login);
+      dispatch({ type: 'GET_USER_AND_REPOS', payload: userData });
+    })();
+  }, [dispatch, params.login]);
 
   const {
     name,
